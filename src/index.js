d2970e676641eaba324e6f1e8bfce5d0b0e1a9ad
Rad1m <radim.simanek@protonmail.ch>
2022-01-29 15:44:13 +0000

    JavaScript and CSS done

--- src/index.js
+++ src/index.js
@@ -1,34 +1,36 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import "./index.css" // this means that file is in same folder
 
 // this is stateless functional component
 // always use return to return JSX
 // use capital letter for function name to let React know that this function is Component
 
-
-// Verion 1
+// main function which is containing child elements (functions)
 function BookList() {
   return(
-    <section>
+    <section className="booklist">
       <Book/>
-      
     </section>
   ) 
 }
 
+const author = "Amazon" // can be inside or outside of Book function
 const Book = () => {
-  return <article>
-    <Image />
-    <Title/>
-    <Author/>
+  const title = "Fire HD TV"
+  return <article className="book">
+    <img src = "https://m.media-amazon.com/images/I/51h3GkZzvnL._AC_SL1000_.jpg" alt = "" />
+    <h1>{title.toUpperCase()}</h1>
+    <h4>{author}</h4>
   </article>
 }
 
-const Image = () => <img src = "https://m.media-amazon.com/images/I/51h3GkZzvnL._AC_SL1000_.jpg" alt = "" width="300" height="350"  />
-
-const Title = () => <h1>Fire HD TV</h1>
-
-const Author = () => <h4>Amazon</h4>
+//styles can be in css file or they can be directly here
+//However the syntax is different becuase of using JavaScript
+//Thats why we use {{}} and all values are in ""
+// "" or '' is same
+// priority is given here so it will overwrite settings from css file
+//const Author = () => <h4 style = {{color:"#617d98", fontSize:"0.75rem", marginTop: "0.25rem"}}>Amazon</h4>
 
 // I need to use self closing tag "</>" otherwise it won't work
 ReactDOM.render(<BookList />, document.getElementById('root'))
